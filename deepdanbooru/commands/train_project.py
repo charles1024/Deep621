fbe00f1f14c9bd754c4b28690071363e6deba8e4
charles1024 <charlesw1024@gmail.com>
2022-03-14 18:02:43 -0700

    removed debug statement

--- deepdanbooru/commands/train_project.py
+++ deepdanbooru/commands/train_project.py
@@ -190,7 +190,7 @@ def train_project(project_path, source_model):
             dataset_wrapper = dd.data.DatasetWrapper(
                 (image_paths, tag_strings), tags, width, height, scale_range=scale_range, rotation_range=rotation_range, shift_range=shift_range)
             dataset = dataset_wrapper.get_dataset(minibatch_size)
-            print(f'dataset len: {len(dataset)} ')
+            #print(f'dataset len: {len(dataset)} ')
             for (x_train, y_train) in dataset:
                 sample_count = x_train.shape[0]
                 print(f'sample cout: {sample_count}, y_train: {y_train}')
